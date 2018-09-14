3b09e5fcb99cd26da585cae4def38e940dec244b
NikiArt <genertyniki@gmail.com>
2018-09-14 10:50:46 +0300

    new project

---
+++ src/main/java/ru/boiko/se/App.java
@@ -0,0 +1,15 @@
+package ru.boiko.se;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+
+    }
+}
